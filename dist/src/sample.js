77dba9e03a51036ccf262aa8f81143d907b2ba8f
Lorentz <91217473+Lauro235@users.noreply.github.com>
2023-01-31 00:12:41 +0000

    remove type: module from package.json => TS include import and export out of the box so need for it and it causes node exe errors

---
+++ dist/src/sample.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    constructor(name) {
+        console.log(`Constructor ${name} calling`);
+    }
+}
+exports.default = Person;
+let myGuy = new Person('Bob');
+//# sourceMappingURL=sample.js.map
